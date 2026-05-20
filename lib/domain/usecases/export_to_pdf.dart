898440eed07add2ec928eb3f91d573a8663b2a6f
MIGUEL ANGEL ANTUNEZ GALINDO <m.antunez94@hotmail.com>
2026-05-20 23:28:40 +0200

    feat: add PDF export and document sharing with share_plus

---
+++ lib/domain/usecases/export_to_pdf.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:path_provider/path_provider.dart';
+import '../entities/scanned_document.dart';
+
+class ExportToPdf {
+  Future<File> call(List<ScannedDocument> documents) async {
+    final dir = await getApplicationDocumentsDirectory();
+    final pdfDir = Directory('${dir.path}/exports');
+    if (!await pdfDir.exists()) await pdfDir.create(recursive: true);
+
+    final pdf = pw.Document();
+
+    for (final doc in documents) {
+      final imageBytes = await File(doc.filePath).readAsBytes();
+      final image = pw.MemoryImage(imageBytes);
+
+      pdf.addPage(
+        pw.Page(
+          pageFormat: PdfPageFormat.a4,
+          build: (_) => pw.Center(child: pw.Image(image, fit: pw.BoxFit.contain)),
+        ),
+      );
+    }
+
+    final outputPath = '${pdfDir.path}/documents_${DateTime.now().millisecondsSinceEpoch}.pdf';
+    final file = File(outputPath);
+    await file.writeAsBytes(await pdf.save());
+    return file;
+  }
+}
